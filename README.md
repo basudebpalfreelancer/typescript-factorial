a56282c615b05f879c5e15fe764523382ae49c94
basudebpalfreelancer <basudeb.pal.freelancer@gmail.com>
2019-11-18 13:44:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-f46ver
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-f46ver)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Import stylesheets
+import "./style.css";
+
+function factorial(n: number): number {
+  if (Number.isInteger(n) && n >= 0)
+    return n > 1 ? n * factorial(n - 1) : 1
+  else
+    throw new Error('Invalid number')
+}
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>${factorial(9)}</h1>`;
